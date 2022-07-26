00b9f7f1d4eb90cd5a83d62517c889c26087f7cf
Jai <53321302+realjai@users.noreply.github.com>
2022-07-26 12:56:37 +0800

    275 - Empathy Legal Hurriane - Add trusted form

--- hurricane.php
+++ hurricane.php
@@ -80,6 +80,7 @@
         }
     </script>
 
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -906,6 +907,21 @@
             }
         });
     </script>
+
+    <!-- TrustedForm -->
+    <script type="text/javascript">
+        (function() {
+            var tf = document.createElement('script');
+            tf.type = 'text/javascript'; tf.async = true;
+            tf.src = ("https:" == document.location.protocol ? 'https' : 'http') + "://api.trustedform.com/trustedform.js?field=xxTrustedFormCertUrl&ping_field=xxTrustedFormPingUrl&l=" + new Date().getTime() + Math.random();
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(tf, s);
+        })();
+    </script>
+    <noscript>
+        <img src="https://api.trustedform.com/ns.gif" />
+    </noscript>
+    <!-- End TrustedForm -->
+
 </body>
 
 </html>
